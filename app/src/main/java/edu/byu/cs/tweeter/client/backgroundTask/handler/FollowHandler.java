9efbeb8db505ab60dca93d2230d68e3d5230126c
andres1142 <afmo2@byu.edu>
2022-09-28 16:40:26 -0600

    Handlers separated from Service classes

--- app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/handler/FollowHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/handler/FollowHandler.java
@@ -4,9 +4,8 @@ import android.os.Bundle;
 
 import edu.byu.cs.tweeter.client.backgroundTask.observer.FollowInteraction;
 import edu.byu.cs.tweeter.client.model.service.FollowService;
-import edu.byu.cs.tweeter.client.backgroundTask.observer.ServiceObserver;
 
-public class FollowHandler extends BackgroundTaskHandler<FollowInteraction> {
+public class FollowHandler extends FollowBackgroundHandler<FollowInteraction> {
 
     public FollowHandler(FollowService.FollowMainObserver observer){
         super(observer);
@@ -14,6 +13,6 @@ public class FollowHandler extends BackgroundTaskHandler<FollowInteraction> {
 
     @Override
     protected void handleSuccessMessage(FollowInteraction observer, Bundle data) {
-        observer.handleSuccessFollowUnfollow(true);
+        observer.handleSuccessFollowUnfollow(false);
     }
 }
